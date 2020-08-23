279f9049c449033c7af77b3659cd69214c2ffd34
rbbanner <rlfoster2018@gmail.com>
2020-08-22 22:29:04 -0500

    Exercises for Day 3 of classes 8/22/20

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,58 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class SchoolPractice {
+
+    public class Course {
+        private String courseName;
+        private Integer courseNumber;
+        private Integer semesterCredit;
+        private String instructor;
+        private ArrayList classRoster = new ArrayList();
+    }
+
+    public class Teacher {
+        private String firstName;
+        private String lastName;
+        private String subject;
+        private Integer yearsTeaching;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String aName) {
+            firstName = aName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String aName) {
+            lastName = aName;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String aSubject) {
+            subject = aSubject;
+        }
+
+        public Integer getYearsTeaching() {
+            return yearsTeaching;
+        }
+
+        public void setStudentID(Integer years) {
+            yearsTeaching = years;
+        }
+
+    }
+
     public static void main(String[] args) {
-        // Instantiate your Student class for part 2 here!
+        Student Richard = new Student("Richard", 007, 1, 4.0);
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,43 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, Integer studentId, Integer credits, Double GPA) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = credits;
+        this.gpa = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentID(Integer anID) {
+        studentId = anID;
+    }
+
+    public Integer getCredits() {
+        return numberOfCredits;
+    }
+
+    public void setCredits(Integer credits) {
+        numberOfCredits = credits;
+    }
+
+    public Double getGPA() {
+        return gpa;
+    }
+
+    public void setGPA(Double newGPA) {
+        gpa = newGPA;
+    }
+
 }
